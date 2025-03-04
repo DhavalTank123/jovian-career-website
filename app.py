9f8c91361855686c65590cf8b9e97f7878f8fdab
DhavalTank123 <151733654+DhavalTank123@users.noreply.github.com>
2025-03-04 13:25:01 +0530

    2nd

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 app = Flask(__name__)   # here app is object and Flask is class we pass argument _name_ = _main_
+
+JOBS = [
+    {
+        "id":"1",
+        "title":"Data Analyst",
+        "location":"Bangaluru,India",
+        "salary":"Rs 10,00,000"
+    },
+     {
+        "id":"2",
+        "title":"Data Scientist",
+        "location":"Delhi,India",
+        "salary":"Rs 15,00,000"
+    },
+     {
+        "id":"3",
+        "title":"Frontend",
+        "location":"Remote",
+
+    },
+      {
+        "id":"4",
+        "title":"Backend",
+        "location":"San Francisco ,USA",
+        "salary":"Rs 20,00,000"
+    }
+]
 @app.route("/")
 def hello_world():
-    return "Hello world!"
+    return render_template('home.html',jobs=JOBS,company_name="Javian")
+
+@app.route("/api/jobs")
+def job_list():
+    return jsonify(JOBS)
 
 if __name__== '__main__':
     app.run(host='127.0.0.1',debug=True)  #app.run is Flask method to run a Flask app
